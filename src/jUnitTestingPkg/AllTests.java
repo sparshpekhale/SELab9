96d712b7af0296ebfcf3910ec16de5fe835c1ed1
Sparsh <sparsh8910@yahoo.com>
2021-04-16 23:13:47 +0530

    Added jUnit Testing Suite

---
+++ src/jUnitTestingPkg/AllTests.java
@@ -0,0 +1,11 @@
+package jUnitTestingPkg;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ testAddNum.class, testAddStr.class})
+public class AllTests {
+
+}
